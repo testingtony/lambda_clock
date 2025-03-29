0f7bae184dc8c1bdb8bab0c1ef238cce7342fe46
Tony Holroyd <testingtony12@gmail.com>
2025-03-29 22:57:22 +0000

    deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-cd $(dirname "$0")/../src
 
-zip --exclude __pycache__ -r ../package.zip *
+cd $(dirname "$0")/..
+
+rm package.zip
+rm -rf package
+pip install --platform manylinux2014_aarch64 --target=package --implementation cp --python-version 3.12 --only-binary=:all: --upgrade .
+cd package
+zip -r ../package.zip . --exclude "**/__pycache__/*" --exclude "__pycache__/*"
+cd ../src
+
+zip  ../package.zip handler.py --update
 aws --profile deployer --region eu-west-1 lambda update-function-code --function-name messenger --zip-file fileb://../package.zip
